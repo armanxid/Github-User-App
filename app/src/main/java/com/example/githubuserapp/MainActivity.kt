07ea38ca78ac8a08e0554e6b1efd0d6d9cb97f26
armanxid <nurarmana@gmail.com>
2022-03-19 08:38:20 +0700

    Navigation
    Add SearchView

--- app/src/main/java/com/example/githubuserapp/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.githubuserapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,6 +33,48 @@ class MainActivity : AppCompatActivity() {
         list.addAll(listUser)
         showRecyclerList()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+//            R.id.menu1 -> {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.fragment_container, MenuFragment())
+//                    .addToBackStack(null)
+//                    .commit()
+//                return true
+//            }
+//            R.id.menu2 -> {
+//                val i = Intent(this, MenuActivity::class.java)
+//                startActivity(i)
+//                return true
+//            }
+            else -> return true
+        }
+
+    }
+
+
+
 
     private val listUser: ArrayList<User>
         get() {
